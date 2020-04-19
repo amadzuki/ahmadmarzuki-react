7bbdefca2b599fc42f1cd636ecaf9c1918c71635
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-04-19 17:04:21 +0700

    experiment on structure

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import "./App.css"
-
-function App() {
-  return <div>Hello, I'm Marzuki</div>
-}
-
-export default App
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function HeroHeading() {
+  return <h1>Hello, I'm Marzuki</h1>
+}
+
+function App() {
+  return (
+    <div>
+      <HeroHeading></HeroHeading>
+
+      <div className="hero"></div>
+    </div>
+  )
+}
+
+export default App
