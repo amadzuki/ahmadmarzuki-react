fd16086803057ee2b971978db92c520bbbb51a1e
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-04-20 23:20:30 +0700

    Implement home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,61 @@
 import React from "react"
 
 function HeroHeading() {
-  return <h1>Hello, I'm Marzuki</h1>
+  return (
+    <h1
+      style={{
+        fontSize: "65px",
+        color: "white",
+        margin: "10px",
+      }}
+    >
+      Hello, I'm Marzuki
+    </h1>
+  )
+}
+
+const HeroButton = () => {
+  return (
+    <div>
+      <button
+        style={{
+          fontFamily: "Roboto",
+          background: "orange",
+          border: "none",
+          borderRadius: "50px",
+          padding: "15px 30px",
+          fontSize: "21px",
+        }}
+      >
+        See My Portfolio
+      </button>
+    </div>
+  )
+}
+
+function HeroImage({ children }) {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        backgroundImage: `url("/images/HeroHome.jpg")`,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {children}
+    </div>
+  )
 }
 
 function App() {
   return (
-    <div>
+    <HeroImage>
       <HeroHeading></HeroHeading>
-
-      <div className="hero"></div>
-    </div>
+      <HeroButton></HeroButton>
+    </HeroImage>
   )
 }
 
