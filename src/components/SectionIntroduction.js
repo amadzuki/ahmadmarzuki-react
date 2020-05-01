0212dc4571e36b0a95924827be72722ee4e497ae
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-05-01 17:30:43 +0700

    Example include image in public and source folder

---
+++ src/components/SectionIntroduction.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const SectionIntrodution = () => {
+  return (
+    <div>
+      <p>
+        Hello, my name is Ahmad Marzuki. I'm a budding web developer. I'm
+        learning React right now.
+      </p>
+      <img src="/images/ahmadmarzuki.jpg" alt="Ahmad Marzuki" />
+    </div>
+  )
+}
+
+export default SectionIntrodution
